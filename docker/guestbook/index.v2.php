6e8368f089d9098eea65a9f96ba49eb793f94416
Caitlin Elfring <celfring@renttherunway.com>
2020-04-23 21:36:26 -0400

    Add environment

--- docker/guestbook/index.v2.php
+++ docker/guestbook/index.v2.php
@@ -1,14 +1,17 @@
+<?php $env = getenv('ENVIRONMENT'); ?>
 <html ng-app="redis">
   <head>
-    <title>Guestbook</title>
+    <title>Guestbook - <?php echo $env; ?></title>
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.12/angular.min.js"></script>
     <script src="controllers.js"></script>
+    <script src="fancy.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.13.0/ui-bootstrap-tpls.js"></script>
   </head>
   <body ng-controller="RedisCtrl">
     <div style="width: 50%; margin-left: 20px">
-      <h2>Guestbook</h2>
+      <h2 fancy>Fancy Guestbook</h2>
+      <h4><?php echo $env; ?></h4>
     <form>
     <fieldset>
     <input ng-model="msg" placeholder="Messages" class="form-control" type="text" name="input"><br>
