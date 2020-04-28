4845a893d167bca9b53988d97186081668026429
Caitlin Elfring <celfring@renttherunway.com>
2020-04-28 12:28:11 -0400

    Support adding webhook to multiple repos
    Only delete ngrok.io/api/webhook hooks

--- configure-gh-webhook.sh
+++ configure-gh-webhook.sh
@@ -2,11 +2,13 @@
 
 test -r .env && source .env
 
-echo "WARNING: This will delete all existing webhooks in your repo!"
+if [ -z "${GITHUB_ACCESS_TOKEN}" ]; then echo "GITHUB_ACCESS_TOKEN env var required!"; exit 1; fi
+if [ -z "${GITHUB_REPOS}" ]; then echo "GITHUB_REPOS env var required! should be space-separated list (ie 'caitlin615/argocd-repo caitlin615/argocd-apps')"; exit 1; fi
+
+echo "WARNING: This will delete all existing 'ngrok.io/api/webhook' webhooks in github repos: ${GITHUB_REPOS}!"
 echo
 
-if [ -z "${GITHUB_ACCESS_TOKEN}" ]; then echo "GITHUB_ACCESS_TOKEN env var required!"; exit 1; fi
-if [ -z "${GITHUB_REPO}" ]; then echo "GITHUB_REPO env var required! (ie caitlin615/argocd-repo)"; exit 1; fi
+GITHUB_REPOS_ARRAY=(${GITHUB_REPOS})
 
 function createHook {
     HOOK_HOST=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
@@ -28,23 +30,28 @@ function createHook {
     }
     }' | jq -cr .)
 
-    curl -s -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
-        -H "Accept: application/vnd.github.v3+json" \
-        -X POST \
-        -d "${HOOK_BODY}" \
-        "https://api.github.com/repos/${GITHUB_REPO}/hooks"
+    for REPO in "${GITHUB_REPOS_ARRAY[@]}"; do
+        curl -s -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
+            -H "Accept: application/vnd.github.v3+json" \
+            -X POST \
+            -d "${HOOK_BODY}" \
+            "https://api.github.com/repos/${REPO}/hooks"
+    done
 }
 
 function deleteHooks {
-    # https://developer.github.com/v3/repos/hooks/#delete-a-hook
-    for i in $(curl -s -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
-        -H "Accept: application/vnd.github.v3+json" \
-        "https://api.github.com/repos/${GITHUB_REPO}/hooks" | jq -r '.[].id' ); do
-
-        curl -s -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
+    for REPO in "${GITHUB_REPOS_ARRAY[@]}"; do
+        # https://developer.github.com/v3/repos/hooks/#delete-a-hook
+        for hook in $(curl -s -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
             -H "Accept: application/vnd.github.v3+json" \
-            -X DELETE \
-            "https://api.github.com/repos/${GITHUB_REPO}/hooks/$i"
+            "https://api.github.com/repos/${REPO}/hooks" | \
+            jq -r '.[] | select(.config.url | endswith(".ngrok.io/api/webhook")) | .id' ); do
+
+            curl -s -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" \
+                -H "Accept: application/vnd.github.v3+json" \
+                -X DELETE \
+                "https://api.github.com/repos/${REPO}/hooks/$hook"
+        done
     done
 }
 
